f07916a54bf4e3931af476f143ea5050fe3e1222
root <root@ks351177.kimsufi.com>
2013-03-05 17:57:00 +0100

    Bla

--- application/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-use MuMVC\Registry;
-
-Registry::instance()->set('db:params', array(
-	'dbname' => 'alex_413x31_com',
-	'username'   => 'alex_413x31',
-	'password'   => 'Aj$.1)_#',
-	'server' => 'localhost',
-));
