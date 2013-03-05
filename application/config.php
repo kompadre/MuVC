a95c42e5277186bf01039a72cc4afd8ae55e7e20
root <root@ks351177.kimsufi.com>
2013-03-05 17:53:43 +0100

    Git Ignore

--- application/config.php
+++ application/config.php
@@ -2,8 +2,8 @@
 use MuMVC\Registry;
 
 Registry::instance()->set('db:params', array(
-	'dbname' => 'zf2',
-	'username'   => 'zf2',
+	'dbname' => 'alex_413x31_com',
+	'username'   => '',
 	'password'   => 'zf2',
 	'server' => 'localhost',
 ));
