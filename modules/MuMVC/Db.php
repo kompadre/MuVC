e101ff4b217dc9d4fbe4412fef6ea3375ba0d056
Alexey Serikov <kompadre@gmail.com>
2013-03-05 18:10:15 +0100

    Error message when connecting to MySQL

--- modules/MuMVC/Db.php
+++ modules/MuMVC/Db.php
@@ -2,12 +2,16 @@
 namespace MuMVC;
 
 require_once MUMVC_ROOT . '/Db/IDbDriver.php';
-
 class Db implements ICacheable {
+
 	protected $driver;
 	public function __construct($driver='mysql') {
 		$driverString = __NAMESPACE__ . '\\Db\\' . ucfirst($driver) . 'Driver';
-		$this->driver = new $driverString(Registry::get('db:params'));
+		try {
+			$this->driver = new $driverString(Registry::get('db:params'));
+		} catch (\Exception $e) {
+			echo $e->getMessage();
+		}
 	}
 	public static function instance($driver='mysql') {
 		parent::instance(__CLASS__, $driver);
