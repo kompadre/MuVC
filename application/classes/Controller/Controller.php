c3ddfe32399d0961eacca7e3ea13d733f829d5af
Alexey Serikov <kompadre@gmail.com>
2013-03-24 12:25:37 +0100

    Quickfix: Controller

--- application/classes/Controller/Controller.php
+++ application/classes/Controller/Controller.php
@@ -5,32 +5,14 @@
    at http://alex.413x31.com/Projects/MuMVC/licence.txt */
  
 namespace Application\Controller;
-use \MuMVC\ActionController;
-use \Application\Model\Album;
-use \MuMVC\Registry;
-use \MuMVC\Db;
+
+use MuMVC\ActionController;
 
 class Controller extends ActionController {
 	public function before() {
 		parent::before();
 	}
 	public function indexAction() {
-		try {
- 			$template = $this->template;
- 			$album = new Album();
- 			$album->query("SELECT * FROM album WHERE 1");
- 			while ($row = $album->fetchAssoc()) {
- 				$template->asigna('artist', $row['artist']);
- 				$template->asigna('title',  $row['title']);
- 				$template->asigna('color', sprintf("%06x", mt_rand(0,0xFFFFFF)));
-	 			$template->parse('row');
- 			}
-			$template->asigna('title', 'My first page in MuMVC');
-		} catch (\Exception $e) {
-				echo "There was an error while executing some query.";
-		}
-	}
-	public function error() {
-		die('ere');
+		$this->template->asigna('title', 'My first page in MuMVC');
 	}
 }
